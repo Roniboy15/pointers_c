74bd717156cba5229c8ebe666823a691d63c7659
Sam Smith <sam@example.com>
2022-12-08 14:18:03 +0200

    ok

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  pointers
+//
+//  Created by Jaron Treyer on 08.12.22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
